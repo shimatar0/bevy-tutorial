8666e5e73c3f44b739bd20d58b465e76186d9770
shimatar0 <shimataro0579@gmail.com>
2023-03-11 17:57:31 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: 1600.0,
+                height: 900.0,
+                title: "Bevy Tutorial".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.top = 1.0;
+    camera.projection.bottom = -1.0;
+    camera.projection.right = 1.0 * RESOLUTION;
+    camera.projection.left = -1.0 * RESOLUTION;
+
+    camera.projection.scaling_mode = ScalingMode::None;
+    commands.spawn(camera);
+}
